b6e74f716e9676c112494f3bc3b5f7ed879d0bad
Alexbarcenas7 <barcenasdavalosalejandro2020@gmail.com>
2024-09-22 16:08:04 -0600

    Update and rename PRUEBA.txt to tarea10/tarea10.java

---
+++ tarea10/tarea10.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String palabraTarea = "supercalifragilisticoespialidoso";
+
+        int longitud = palabraTarea.length();
+        String tresDoce = palabraTarea.substring(5, 15);
+        int primeraOcurrenciaI = palabraTarea.lastIndexOf('i');
+        int ultimaOcurrenciaO = palabraTarea.lastIndexOf('o');
+
+
+    }
+}
