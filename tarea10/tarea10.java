e606605624fd9448a35098cd4bcae99d2800093c
Alexbarcenas7 <barcenasdavalosalejandro2020@gmail.com>
2024-09-23 22:12:12 -0600

    Actualizar tarea10.java

--- tarea10/tarea10.java
+++ tarea10/tarea10.java
@@ -7,7 +7,13 @@ public class Main {
         String tresDoce = palabraTarea.substring(5, 15);
         int primeraOcurrenciaI = palabraTarea.lastIndexOf('i');
         int ultimaOcurrenciaO = palabraTarea.lastIndexOf('o');
+        System.out.println("Última ocurrencia de 'o': " + palabra.lastIndexOf('o'));
 
+        System.out.println("En minúsculas: " + palabra.toLowerCase());
+        
+        System.out.println("Subcadena desde la posición 7: " + palabra.substring(6));
+
+        System.out.println("Última ocurrencia de 'li': " + palabra.lastIndexOf("li"));
 
     }
 }
